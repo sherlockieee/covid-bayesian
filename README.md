a5e67ea003d23dd023b7d7b75135c596102add69
sherlockieee <phuonghatrannguyen00@gmail.com>
2021-12-13 19:11:37 +0100

    final project first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CS152 Final Project
+
+A final project using Streamlit, Hedgehog Bayesian Network and a COVID outbreak in our current resident hall.
+
+With love and support by (Esther Yang)[https://github.com/esther119].
---
+++ app.py
@@ -0,0 +1,36 @@
+import hedgehog as hh
+import pandas as pd
+
+bn = hh.BayesNet(
+    ("Gabriel", "Ha"), ("Ha", "Esther"), ("Gabriel", "Anais"), ("Ha", "Anais")
+)
+
+# P(Gabriel gets COVID)
+bn.P["Gabriel"] = pd.Series({False: 0.5, True: 0.5})
+
+# P(Ha gets COVID | Gabriel gets COVID)
+bn.P["Ha"] = pd.Series(
+    {(True, True): 0.7, (True, False): 0.3, (False, True): 0.4, (False, False): 0.6}
+)
+
+# P(Anais gets COVID | Gabriel gets COVID, Ha gets COVID)
+bn.P["Anais"] = pd.Series(
+    {
+        (True, True, True): 0.92,
+        (True, True, False): 0.08,
+        (True, False, True): 0.3,
+        (True, False, False): 0.7,
+        (False, True, True): 0.44,
+        (False, True, False): 0.56,
+        (False, False, True): 0.1,
+        (False, False, False): 0.9,
+    }
+)
+
+# P(Esther gets COVID | Ha gets COVID)
+bn.P["Esther"] = pd.Series(
+    {(True, True): 0.94, (True, False): 0.06, (False, True): 0.43, (False, False): 0.56}
+)
+
+bn.prepare()
+print(bn.query("Anais", event={"Gabriel": True}))
